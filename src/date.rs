9e0ca3aa0836b66857b297813a6e48498ba80da5
Robert Galluccio <robertnw5@gmail.com>
2025-06-22 22:40:14 -0400

    Added macro for DateTime constructor

--- src/date.rs
+++ src/date.rs
@@ -2,6 +2,35 @@ use std::fmt::Display;
 
 use crate::{tag::value::ParsedTagValue, utils::parse_date_time_bytes};
 
+#[macro_export]
+macro_rules! date_time {
+    ($Y:literal-$M:literal-$D:literal T $h:literal:$m:literal:$s:literal) => {
+        DateTime {
+            date_fullyear: $Y,
+            date_month: $M,
+            date_mday: $D,
+            time_hour: $h,
+            time_minute: $m,
+            time_second: $s,
+            timezone_offset: Default::default(),
+        }
+    };
+    ($Y:literal-$M:literal-$D:literal T $h:literal:$m:literal:$s:literal $x:literal:$y:literal) => {
+        DateTime {
+            date_fullyear: $Y,
+            date_month: $M,
+            date_mday: $D,
+            time_hour: $h,
+            time_minute: $m,
+            time_second: $s,
+            timezone_offset: DateTimeTimezoneOffset {
+                time_hour: $x,
+                time_minute: $y,
+            },
+        }
+    };
+}
+
 #[derive(Debug, PartialEq, Clone, Copy)]
 pub struct DateTime {
     pub date_fullyear: u32,
@@ -91,18 +120,7 @@ mod tests {
     #[test]
     fn no_timezone() {
         assert_eq!(
-            DateTime {
-                date_fullyear: 2025,
-                date_month: 6,
-                date_mday: 4,
-                time_hour: 13,
-                time_minute: 50,
-                time_second: 42.148,
-                timezone_offset: DateTimeTimezoneOffset {
-                    time_hour: 0,
-                    time_minute: 0
-                }
-            },
+            date_time!(2025-06-04 T 13:50:42.148),
             parse("2025-06-04T13:50:42.148Z").unwrap()
         );
     }
@@ -110,18 +128,7 @@ mod tests {
     #[test]
     fn plus_timezone() {
         assert_eq!(
-            DateTime {
-                date_fullyear: 2025,
-                date_month: 6,
-                date_mday: 4,
-                time_hour: 13,
-                time_minute: 50,
-                time_second: 42.148,
-                timezone_offset: DateTimeTimezoneOffset {
-                    time_hour: 3,
-                    time_minute: 0
-                }
-            },
+            date_time!(2025-06-04 T 13:50:42.148 03:00),
             parse("2025-06-04T13:50:42.148+03:00").unwrap()
         );
     }
@@ -129,18 +136,7 @@ mod tests {
     #[test]
     fn negative_timezone() {
         assert_eq!(
-            DateTime {
-                date_fullyear: 2025,
-                date_month: 6,
-                date_mday: 4,
-                time_hour: 13,
-                time_minute: 50,
-                time_second: 42.148,
-                timezone_offset: DateTimeTimezoneOffset {
-                    time_hour: -1,
-                    time_minute: 30
-                }
-            },
+            date_time!(2025-06-04 T 13:50:42.148 -01:30),
             parse("2025-06-04T13:50:42.148-01:30").unwrap()
         );
     }
@@ -148,18 +144,7 @@ mod tests {
     #[test]
     fn no_fractional_seconds() {
         assert_eq!(
-            DateTime {
-                date_fullyear: 2025,
-                date_month: 6,
-                date_mday: 4,
-                time_hour: 13,
-                time_minute: 50,
-                time_second: 42.0,
-                timezone_offset: DateTimeTimezoneOffset {
-                    time_hour: 0,
-                    time_minute: 0
-                }
-            },
+            date_time!(2025-06-04 T 13:50:42.0),
             parse("2025-06-04T13:50:42Z").unwrap()
         );
     }
@@ -168,18 +153,7 @@ mod tests {
     fn string_from_single_digit_dates_should_be_valid() {
         assert_eq!(
             String::from("2025-06-04T13:50:42.123Z"),
-            String::from(DateTime {
-                date_fullyear: 2025,
-                date_month: 6,
-                date_mday: 4,
-                time_hour: 13,
-                time_minute: 50,
-                time_second: 42.123,
-                timezone_offset: DateTimeTimezoneOffset {
-                    time_hour: 0,
-                    time_minute: 0
-                }
-            })
+            String::from(date_time!(2025-06-04 T 13:50:42.123))
         )
     }
 
@@ -187,18 +161,7 @@ mod tests {
     fn string_from_no_fractional_seconds_should_still_be_3_decimals_precise() {
         assert_eq!(
             String::from("2025-06-04T13:50:42.000Z"),
-            String::from(DateTime {
-                date_fullyear: 2025,
-                date_month: 6,
-                date_mday: 4,
-                time_hour: 13,
-                time_minute: 50,
-                time_second: 42.0,
-                timezone_offset: DateTimeTimezoneOffset {
-                    time_hour: 0,
-                    time_minute: 0
-                }
-            })
+            String::from(date_time!(2025-06-04 T 13:50:42.0))
         )
     }
 
@@ -206,18 +169,7 @@ mod tests {
     fn string_from_single_digit_times_should_be_valid() {
         assert_eq!(
             String::from("2025-12-25T04:00:02.000Z"),
-            String::from(DateTime {
-                date_fullyear: 2025,
-                date_month: 12,
-                date_mday: 25,
-                time_hour: 4,
-                time_minute: 0,
-                time_second: 2.0,
-                timezone_offset: DateTimeTimezoneOffset {
-                    time_hour: 0,
-                    time_minute: 0
-                }
-            })
+            String::from(date_time!(2025-12-25 T 04:00:02.000))
         )
     }
 
@@ -225,18 +177,7 @@ mod tests {
     fn string_from_negative_time_offset_should_be_valid() {
         assert_eq!(
             String::from("2025-06-04T13:50:42.123-05:00"),
-            String::from(DateTime {
-                date_fullyear: 2025,
-                date_month: 6,
-                date_mday: 4,
-                time_hour: 13,
-                time_minute: 50,
-                time_second: 42.123,
-                timezone_offset: DateTimeTimezoneOffset {
-                    time_hour: -5,
-                    time_minute: 0
-                }
-            })
+            String::from(date_time!(2025-06-04 T 13:50:42.123 -05:00))
         )
     }
 
@@ -244,18 +185,7 @@ mod tests {
     fn string_from_positive_offset_should_be_valid() {
         assert_eq!(
             String::from("2025-06-04T13:50:42.100+01:00"),
-            String::from(DateTime {
-                date_fullyear: 2025,
-                date_month: 6,
-                date_mday: 4,
-                time_hour: 13,
-                time_minute: 50,
-                time_second: 42.1,
-                timezone_offset: DateTimeTimezoneOffset {
-                    time_hour: 1,
-                    time_minute: 0
-                }
-            })
+            String::from(date_time!(2025-06-04 T 13:50:42.100 01:00))
         )
     }
 
@@ -263,18 +193,64 @@ mod tests {
     fn string_from_positive_offset_non_zero_minutes_should_be_valid() {
         assert_eq!(
             String::from("2025-06-04T13:50:42.010+06:30"),
-            String::from(DateTime {
+            String::from(date_time!(2025-06-04 T 13:50:42.010 06:30))
+        )
+    }
+
+    #[test]
+    fn date_time_macro_should_work_with_no_offset() {
+        assert_eq!(
+            date_time!(2025-06-22 T 22:13:42.000),
+            DateTime {
                 date_fullyear: 2025,
                 date_month: 6,
-                date_mday: 4,
-                time_hour: 13,
-                time_minute: 50,
-                time_second: 42.01,
+                date_mday: 22,
+                time_hour: 22,
+                time_minute: 13,
+                time_second: 42.0,
                 timezone_offset: DateTimeTimezoneOffset {
-                    time_hour: 6,
+                    time_hour: 0,
+                    time_minute: 0
+                }
+            }
+        );
+    }
+
+    #[test]
+    fn date_time_macro_should_work_with_positive_offset() {
+        assert_eq!(
+            date_time!(2025-06-22 T 22:13:42.000 01:00),
+            DateTime {
+                date_fullyear: 2025,
+                date_month: 6,
+                date_mday: 22,
+                time_hour: 22,
+                time_minute: 13,
+                time_second: 42.0,
+                timezone_offset: DateTimeTimezoneOffset {
+                    time_hour: 1,
+                    time_minute: 0
+                }
+            }
+        );
+    }
+
+    #[test]
+    fn date_time_macro_should_work_with_negative_offset() {
+        assert_eq!(
+            date_time!(2025-06-22 T 22:13:42.000 -01:30),
+            DateTime {
+                date_fullyear: 2025,
+                date_month: 6,
+                date_mday: 22,
+                time_hour: 22,
+                time_minute: 13,
+                time_second: 42.0,
+                timezone_offset: DateTimeTimezoneOffset {
+                    time_hour: -1,
                     time_minute: 30
                 }
-            })
-        )
+            }
+        );
     }
 }
